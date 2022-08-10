35b042da187a6cd6f9ae682d01d178242604fd9f
Vit Onischenko <slonik0775@gmail.com>
2022-08-10 03:18:38 +0300

    второй вариант работы с апи НБУ

--- src/convertor/Convertor.js
+++ src/convertor/Convertor.js
@@ -1,31 +1,51 @@
 import './convertor.css'
 import {useEffect, useState} from "react";
 
-
-export const Convertor = () => {
-    const [exchange, setExchange] =useState([]);
-    const [course, setCourse] = useState('Choose currency');
+export  function Convertor() {
+    const [curr, setCurr] = useState(0);
+    const [input, setInput] = useState(0);
+    const [currName, setCurrName] = useState("");
+    const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
-            .then(res => res.json())
-            .then(setExchange);
-    }, [])
+        fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
+            .then((res) => res.json())
+            .then((data) => setItems(data));
+    }, []);
 
-    const convert = (currencyCode) => {
-        const target = exchange.find(item => item.cc === currencyCode);
-        setCourse(`1${target.cc} = ${target.rate}UAH`)
+    function searchVal(name) {
+        items.forEach((i) => {
+            if (i.cc === name) {
+                setCurr((input / i.rate).toFixed(1));
+                setCurrName(i.cc);
+            }
+        });
+    }
+
+    function setInputValue({ target }) {
+        const value = Number(target.value);
+        setInput(value);
     }
 
     return (
-        <div class="app">
-            <div class="counter">{course}</div>
-            <div class="controls">
-                <button onClick={() => convert('USD')}>USD</button>
-                <button onClick={() => convert('RUB')}>RUB</button>
-                <button onClick={() => convert('EUR')}>EUR</button>
-                <button onClick={() => convert('GBP')}>GBP</button>
+        <div className="app">
+            <div className="out">
+                <div className="counter">
+                    <input type="text" onChange={setInputValue} />
+                    <span>UAN</span>
+                </div>
+                <div>=</div>
+                <div className="counter">
+                    {curr}
+                    <span>{currName}</span>
+                </div>
+            </div>
+            <div className="controls">
+                <button onClick={() => searchVal("USD")}>USD</button>
+                <button onClick={() => searchVal("EUR")}>EUR</button>
+                <button onClick={() => searchVal("RUB")}>RUB</button>
+                <button onClick={() => searchVal("DKK")}>DKK</button>
             </div>
         </div>
-    )
+    );
 }
